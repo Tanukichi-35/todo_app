eb01635b396548bc2ccdf618310e40d09d624553
Tanukichi-35 <yo-fu@outlook.jp>
2024-01-16 08:28:37 +0900

    add model,controller,table

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [
+    TodoController::class, 'index'
+]);
