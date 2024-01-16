a93f4489d380960df26a579e0787029e5754c253
Tanukichi-35 <yo-fu@outlook.jp>
2024-01-16 21:44:04 +0900

    add message function

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
     public function index(){
         $todos = Todo::all();
-        return view('index', ['todos' => $todos]);
+        return view('index', compact('todos'));
     }
 
-    public function store(Request $request){
+    public function store(TodoRequest $request){
         $todo = $request->all();
+        // dd($todo);
         Todo::create($todo);
-        return redirect('/');
+        return redirect('/')->with('success','Todoを作成しました');
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -7,9 +7,39 @@
 @section('content')
 <div class="todo-form__content">
   <div class="todo-form__message">
-    <ul>
-      <li>Todoを作成しました</li>
-    </ul>
+    {{--
+      @if (count($messages) > 0)
+      <ul>
+        @foreach ($messages->all() as $message)
+        <li class="list__messages">{{$message}}</li>
+        @endforeach
+      </ul>
+      @endif
+      @if (count($error_messages) > 0)
+      <ul>
+        @foreach ($error_messages->all() as $error_message)
+        <li class="list__error_messages">{{$error_message}}</li>
+        @endforeach
+      </ul>
+      @endif
+    --}}
+
+
+    @if (session('success'))
+      <ul>
+        <li class="list__messages">{{ session('success') }}</li>
+      </ul>
+    @elseif (count($errors) > 0)
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li class="list__error_messages">{{$error}}</li>
+        @endforeach
+      </ul>
+    @endif
+
+    {{-- <ul>
+      <li class="list__messages">Todoを作成しました</li>
+    </ul> --}}
   </div>
   <form class="form" action="/todos" method="post">
     @csrf
@@ -17,26 +47,26 @@
       <input class="form__todo-input" type="text" name="content" placeholder="パンを買う" value="{{old ('content')}}" />
       <button class="form__todo-button" type="submit">作成</button>
     </div>
+    <table class="table_show-todo">
+      <tr>
+        <th class="table_show-todo-column">Todo</th>
+        <th></th>
+        <th></th>
+      </tr>
+      @foreach ($todos as $todo)
+      <tr>
+        <td>
+          {{$todo->get()}}
+        </td>
+        <td>
+          <button class="table__button update">更新</button>
+        </td>
+        <td>
+          <button class="table__button delete">削除</button>
+        </td>
+      </tr>
+      @endforeach
+    </table>
   </form>
-  <table class="table_show-todo">
-    <tr>
-      <th class="table_show-todo-column">Todo</th>
-      <th></th>
-      <th></th>
-    </tr>
-    @foreach ($todos as $todo)
-    <tr>
-      <td>
-        {{$todo->get()}}
-      </td>
-      <td>
-        <button class="table__button update">更新</button>
-      </td>
-      <td>
-        <button class="table__button delete">削除</button>
-      </td>
-    </tr>
-    @endforeach
-  </table>  
 </div>
 @endsection
